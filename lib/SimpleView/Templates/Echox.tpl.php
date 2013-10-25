69a60e946dbbf8c696e596df88ad7f3e2610a396
César D. Rodas <crodas@php.net>
2013-10-25 16:35:00 -0300

    fixed issue with echox ({{{ }}}) empty values

--- lib/SimpleView/Templates/Echox.tpl.php
+++ lib/SimpleView/Templates/Echox.tpl.php
@@ -1,4 +1,1 @@
-$__temporary = {{$token[1]}};
-if (!empty($__temporary)) {
-    echo htmlentities($__temporary, ENT_QUOTES, 'UTF-8', false);
-}
+echo htmlentities({{$token[1]}}, ENT_QUOTES, 'UTF-8', false);

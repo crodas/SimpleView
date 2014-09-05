0e9d94c946a22fdfdd9b34e916118f3befa94cf4
César D. Rodas <crodas@php.net>
2014-09-05 16:28:18 -0400

    Fixed issue at parsing foreach statements

--- lib/SimpleView/Templates/Foreach.tpl.php
+++ lib/SimpleView/Templates/Foreach.tpl.php
@@ -1,5 +1,7 @@
 foreach({{$token[1]}}) {
-    @set($vars, explode("=>", explode("as", $token[1], 2)[1]))
+    @set($vars, preg_split("/\bas\b/", $token[1], 2))
+    @set($vars, explode("=>", $vars[1]))
+
     @foreach($vars as $var)
         $this->context[{{@substr(trim($var),1)}}] = {{trim($var)}};
     @end
